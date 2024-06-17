8b1140378e4c6623b969cd3b6926a035903a5eeb
woota <woota@TaeKyun>
2024-06-17 11:33:58 +0900

    Start Spring !

--- sbb/src/main/java/com/mysite/sbb/HelloLombok.java
+++ sbb/src/main/java/com/mysite/sbb/HelloLombok.java
@@ -1,16 +1,17 @@
-package com.mysite.sbb; 
-import lombok.Getter; 
-import lombok.RequiredArgsConstructor; 
+package com.mysite.sbb;
 
-@RequiredArgsConstructor 
-@Getter 
-public class HelloLombok { 
-    private final String hello; 
-    private final int lombok; 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
-    public static void main(String[] args) { 
-        HelloLombok helloLombok = new HelloLombok("헬로", 5); 
-        System.out.println(helloLombok.getHello()); 
-        System.out.println(helloLombok.getLombok()); 
-    } 
+@RequiredArgsConstructor
+@Getter
+public class HelloLombok {
+	private final String hello;
+	private final int lombok;
+
+	public static void main(String[] args) {
+		HelloLombok helloLombok = new HelloLombok("헬로", 5);
+		System.out.println(helloLombok.getHello());
+		System.out.println(helloLombok.getLombok());
+	}
 }
